488dc493e62fd21e08a7caaf5bf02a2097e1eb21
NALNESSS <nalendra.farshad@gmail.com>
2026-09-16 22:02:27 +0700

    tutor

--- tutor3.cpp
+++ tutor3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int i, j, k;
     for (i = 0; i <= 4; i++){
-        for (j = 4; j >= i; j--){
+        for (j = 6; j >= i; j--){
             cout << " ";
         }
         for (k = 0; k<=i; k++){
@@ -12,14 +12,33 @@ int main(){
         }
     cout << endl;
     }
-    for (i = 1; i <= 4; i++){
+    for (i = 3; i <= 6; i++){
         for (j = 0; j <= i; j++){
             cout << " ";
         }
-        for (k = 4; k >= i; k--){
+        for (k = 6; k >= i; k--){
             cout << "* ";
         }
     cout << endl;
     }
+    for (i = 1; i <= 5; i++){
+        for (j = 0; j <= i; j++){
+            cout << " ";
+        }
+        cout << "XX";
+        for (k = 4; k >= i; k--){
+            cout << " ";
+        }
+        for (k = 3; k >= i; k--){
+            cout << " ";
+        }
+        cout << "XX" << endl;
+    }
     return 0;
 }
+
+/*xx        xx 7
+   xx      xx 5
+    xx    xx 3
+     xx  xx 1
+      xxxx */
